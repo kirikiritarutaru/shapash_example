a7403aaad37bda2dd73547f12012c76b686228fc
taru <kirikia@gmail.com>
2021-07-20 21:43:50 +0900

    update

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Shapashのサンプルコード
 
 # 参考
 - [Shapash: Making ML Models Understandable by Everyone](https://pub.towardsai.net/shapash-making-ml-models-understandable-by-everyone-8f96ad469eb3)
+- [Shapash 1.3.2, announcing new features for more auditable AI](https://towardsdatascience.com/shapash-1-3-2-announcing-new-features-for-more-auditable-ai-64a6db71c919)
--- shapash_ex.py
+++ shapash_ex.py
@@ -8,8 +8,10 @@ from sklearn.model_selection import train_test_split
 
 def main():
     house_df, house_dict = data_loading('house_prices')
-    print(house_df.head(3))
-    print(f'house_dict: {house_dict}')
+    print_data = False
+    if print_data:
+        print(house_df.head(3))
+        print(f'house_dict: {house_dict}')
 
     y_df = house_df['SalePrice'].to_frame()
     X_df = house_df[house_df.columns.difference(['SalePrice'])]
@@ -48,7 +50,30 @@ def main():
     )
 
     app = xpl.run_app()
-    # 記載のurlを開く
+    # urlを開く
+    # (左上) Features Importance: 各特徴の重要度のプロット
+    # (右下) Contribution plot: FIで選択した特徴の予測への(局所的な)寄与度合い
+    # (右上) Selection Table: データのテーブル
+    # (右下) Local plot: テーブルで選択したデータにおいて、どの特徴が予測値に最も貢献するかを表示
+
+    # 終了する場合
+    # app.kill()
+
+    # 選択したデータの部分集合の特徴の重要度を表示
+    disp_on_jupyter = False
+    if disp_on_jupyter:
+        subset = [168, 54, 995, 799, 310, 322, 1374,
+                  1106, 232, 645, 1170, 1229, 703, 66,
+                  886, 160, 191, 1183, 1037, 991, 482,
+                  725, 410, 59, 28, 719, 337, 36]
+        xpl.plot.features_importance(selection=subset)
+        xpl.plot.contribution_plot("OverallQual")
+        xpl.filter(max_contrib=8, threshold=100)
+        xpl.plot.local_plot(index=560)
+        xpl.filter(max_contrib=3, threshold=1000)
+        summary_df = xpl.to_pandas()
+        print(summary_df.head())
+        xpl.plot.compare_plot(row_num=[0, 1, 2, 3, 4], max_features=8)
 
 
 if __name__ == '__main__':
